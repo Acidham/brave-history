2447e40e133712ff79f6a04c6b88bcfa3116cc61
Acidham <acidham@gmail.com>
2019-10-01 11:16:39 +0200

    Fix type error

--- src/brave_bookmarks.py
+++ src/brave_bookmarks.py
@@ -8,11 +8,12 @@ from Alfred import Tools as Tools
 BRAVE_BOOKMARKS = '/Library/Application Support/BraveSoftware/Brave-Browser/Default/Bookmarks'
 BRAVE_DEV_BOOKMARKS = '/Library/Application Support/BraveSoftware/Brave-Browser-Dev/Default/Bookmarks'
 
+
 def get_all_urls(the_json):
     def extract_data(data):
-        if data['type'] == 'url':
+        if data.get('type') == 'url':
             urls.append({'name': data['name'], 'url': data['url']})
-        if data['type'] == 'folder':
+        if data.get('type') == 'folder':
             the_children = data['children']
             get_container(the_children)
 
@@ -21,7 +22,7 @@ def get_all_urls(the_json):
             for i in o:
                 extract_data(i)
         if isinstance(o, dict):
-            for k,i in o.items():
+            for k, i in o.items():
                 extract_data(i)
     urls = list()
     get_container(the_json)
@@ -37,9 +38,11 @@ def path_to_bookmarks():
     elif os.path.isfile(bm_dev):
         return bm_dev
 
+
 def get_json_from_file(file):
-    with open(file,'r') as bm_file:
+    with open(file, 'r') as bm_file:
         return json.load(bm_file)['roots']
+
 
 wf = Items()
 query = Tools.getArgv(1) if Tools.getArgv(1) is not None else str()
@@ -49,8 +52,8 @@ if bookmarks_file is not None:
     bm_json = get_json_from_file(bookmarks_file)
     bookmarks = get_all_urls(bm_json)
     for bm in bookmarks:
-        name = bm['name']
-        url = bm['url']
+        name = bm.get('name')
+        url = bm.get('url')
         if query == str() or query.lower() in name.lower():
             wf.setItem(
                 title=name,
